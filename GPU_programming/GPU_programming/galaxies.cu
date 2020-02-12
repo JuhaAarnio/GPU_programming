a22691c3334431666cfa75ef8b0cbc6028a28944
Juha Aarnio <juha.aarnio@live.fi>
2020-02-12 23:01:29 +0200

    Added more VS things to ignore

--- GPU_programming/GPU_programming/galaxies.cu
+++ GPU_programming/GPU_programming/galaxies.cu
@@ -1,22 +1,20 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
 #include <stdio.h>
 
 const int blockSize = 32;
 const int N = 32;
 const int GALAXY_COUNT = 100000;
 
-__global__;
-void calculateAngles(char* a, int* b) {
-
+__global__ void calculateAngles(char* a, int* b) {
+    
 }
 
-void readFile(char* filename) {
+char* readFile(char* filename) {
     int i = 0;
     FILE* fp;
-    char* coordinates;
+    char* coordinates{};
     char* galaxies[GALAXY_COUNT];
     fp = fopen(filename, "r");
     if (fp == NULL) {
@@ -26,4 +24,10 @@ void readFile(char* filename) {
         galaxies[i] = coordinates;
         i++;
     }
+    return galaxies;
+}
+
+int main() {
+    char* realGalaxies = readFile("data_100k_arcmin.txt");
+    char* syntheticGalaxies = readFile("flat_100k_arcmin.txt");
 }
