bbb491f89555be54d449f154b3d7a8828df8635a
Juha Aarnio <juha.aarnio@live.fi>
2020-02-18 23:53:06 +0200

    Updated galaxies.cu
    Added very basic calculation GPU operation. Fixed the GPU function by moving out misplaced allocation operations.

--- GPU_programming/GPU_programming/galaxies.cu
+++ GPU_programming/GPU_programming/galaxies.cu
@@ -3,15 +3,19 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 const int blockSize = 32;
-const int N = 32;
 const int GALAXY_COUNT = 200000;
 
-__global__ void calculateAngles(char* a, int* b) {
-    float* d_realGalaxies; cudaMalloc(&d_realGalaxies, 100000 * sizeof(float));
-    float* d_syntheticGalaxies; cudaMalloc(&d_syntheticGalaxies, 100000 * sizeof(float));
+__global__ void calculateAngles(float* d_realGalaxies, float* d_syntheticGalaxies, float* d_dotProducts) {
+    float dotProduct = 0;
+    for (int i = 0; i < 100000; i++)
+    {
+        dotProduct = d_realGalaxies[i] * d_syntheticGalaxies[i];
+        d_dotProducts[i] = dotProduct;
+    }
 }
 
 float* readFile(char* filename) {
@@ -34,6 +38,24 @@ float* readFile(char* filename) {
 }
 
 int main() {
+    int arraySize = (GALAXY_COUNT / 2) * sizeof(float);
     float* h_realGalaxies = readFile("data_100k_arcmin.txt");
     float* h_syntheticGalaxies = readFile("flat_100k_arcmin.txt");
+    float* h_dotProducts = new float[arraySize];
+    float* d_realGalaxies; cudaMalloc(&d_realGalaxies, arraySize);
+    float* d_syntheticGalaxies; cudaMalloc(&d_syntheticGalaxies, arraySize);
+    float* d_dotProducts; cudaMalloc(&d_dotProducts, arraySize);
+    // Intializing the CUDA computation kernel
+    dim3 blockDimension(blockSize, 1);
+    dim3 gridDimension(1, 1);
+    cudaMemcpy(d_realGalaxies, h_realGalaxies, 100000 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_syntheticGalaxies, h_syntheticGalaxies, 100000 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_dotProducts, h_dotProducts, 100000 * sizeof(float), cudaMemcpyHostToDevice);
+    // Execute the function on GPU
+    calculateAngles<<<blockDimension, gridDimension>>>(d_realGalaxies, d_syntheticGalaxies, d_dotProducts);
+    cudaMemcpy(h_dotProducts, d_dotProducts, arraySize, cudaMemcpyDeviceToHost);
+    for (int i = 0; i < 100000; i++)
+    {
+        printf("%f \n", h_dotProducts[i]);
+    }
 }
